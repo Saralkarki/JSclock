2bd06ebca7c025cbb949c1d15c7afbc5df5ccd26
Saral Karki <Saral@Apples-MacBook-Air.local>
2019-02-28 21:56:03 +0545

    created a clock in javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minute = now.getMinutes();
+    const hour = now.getHours();
+    const secondDegrees = ((seconds/60)* 360) + 90;
+    const minuteDegrees = ((minute/60)* 360)+ 90;
+    const hourDegrees = ((hour/12)* 360)+ 90;
+    const secondHand = document.querySelector("#second");
+    const minuteHand = document.querySelector("#minute");
+    const hourHand = document.querySelector("#hour");
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    console.log(hour,minute,seconds);
+}
+setInterval(setDate,1000);
